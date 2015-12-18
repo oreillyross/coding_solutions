c4400f098832eb0365990e45b1207f1529c1709e
Ross O'Reilly <oreillyross@gmail.com>
2015-12-18 14:41:03 +0000

    js solution

--- list_comprehensions/pythagorean_triples.js
+++ list_comprehensions/pythagorean_triples.js
@@ -12,4 +12,45 @@
  // Haskell has the most elegant solution as lists are Monads 
  // pyth n = [(x,y,z) | x <- [1..n], y <- [x..n], z <- [y..n], x^2 + y^2 == z^2]
  
- http://rosettacode.org/wiki/List_comprehensions#JavaScript
+ // ES5 version
+ 
+ console.log(
+ 
+ (function(n) {
+ 
+ // Using a list monad directly, without list comprehension notation
+ 
+  return mb( rng(1, n),     function(x) {
+  return mb( rng(1 + x, n), function(y) {
+  return mb( rng(1 + y, n), function(z) {
+    
+    return (x * x + y * y === z * z) ? mReturn([x, y, z]) : []; 
+  
+  })})});
+   
+ /* ***************************************** */
+ 
+     // Monadic bind (chain) for lists
+     function mb(xs, f) {
+      
+      return [].concat.apply([], xs.map(f)) 
+     
+     }
+     
+     // Monadic return (inject) for lists
+     function mReturn(a) {
+       return [a];
+     }
+     
+     // Range
+     function rng(m, n) {
+      
+      return Array.apply(null, Array(n - m + 1)).map(
+        function (x, i) { return m + i; }
+        );
+     }
+ 
+ })(100)
+ 
+ );
+ 
