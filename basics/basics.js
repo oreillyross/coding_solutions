758d70bb18abbec4fc4626f05f3c5c2f6b1c5741
Ross O'Reilly <oreillyross@gmail.com>
2015-12-24 07:27:17 +0000

    primefactor function

--- basics/basics.js
+++ basics/basics.js
@@ -28,6 +28,31 @@ console.log("137 is a prime:  " + isPrime(137));
 console.log("237 is a prime:  " + isPrime(237));
 
 
+/* How could you find all prime factors of a number
+  Run a while loop. start dividing by two and if not divisible increase divider until u r done
+*/
+
+function primeFactors(n) {
+    var factors = [];
+    var divisor = 2;
+    
+    while (n > 2) {
+        if (n % divisor === 0) {
+            factors.push(divisor);
+            n = n / divisor;
+        } else {
+            divisor++;
+        }
+    }
+    
+    return factors;
+}
+
+// testing the function
+
+console.log(primeFactors(69));
+
+
 
 
 
