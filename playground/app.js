b67ae5e4717935ef0dc05a63b09ff6f39e1cce02
Ross O'Reilly <oreillyross@gmail.com>
2015-12-29 13:52:18 +0000

    js recursion script

---
+++ playground/app.js
@@ -0,0 +1,12 @@
+const description = (nameAndOccupation) => {
+  if (nameAndOccupation.length < 2) {
+    return ["", "occupation missing"]
+  }
+  else {
+    const [[first, last], occupation] = nameAndOccupation;
+  
+    return [`${first} is a ${occupation}`, "ok"];
+  }
+}
+
+const [reg, status] = description([["Reginald", "Braithwaite"], "programmer"]);
