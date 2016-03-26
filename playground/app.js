b5419e0c2733d944cb3e9b2de99cba72e2546b88
Ross O'Reilly <oreillyross@gmail.com>
2016-03-26 20:40:24 +0100

    merge

--- playground/app.js
+++ playground/app.js
@@ -1,13 +1,1 @@
-const description = (nameAndOccupation) => {
-  if (nameAndOccupation.length < 2) {
-    return ["", "occupation missing"]
-  }
-  else {
-    const [[first, last], occupation] = nameAndOccupation;
-  
-    return [`${first} is a ${occupation}`, "ok"];
-  }
-}
-
-const [reg, status] = description([["Reginald", "Braithwaite"], "programmer"]);
 
