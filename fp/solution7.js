cba4a2067af8d7c2bec74a0ff6dabd1f4297c3ca
Ross O'Reilly <oreillyross@gmail.com>
2016-05-31 08:28:58 +0000

    more solutions

---
+++ fp/solution7.js
@@ -0,0 +1,21 @@
+  function reduce(arr, fn, initial) {
+ 
+      if(arr.length) return reduce(arr.slice(1), fn, fn(initial, arr[0]))
+      else return initial
+ 
+ }
+    
+    module.exports = reduce
+    
+/*
+
+Official version
+
+   function reduce(arr, fn, initial) {
+      return (function reduceOne(index, value) {
+        if (index > arr.length - 1) return value // end condition
+        return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
+      })(0, initial) // IIFE. kick off recursion with initial values
+    }
+    
+*/
