9575bbc3f978509148044d55cdb8ad324c5e8343
Ross O'Reilly <oreillyross@gmail.com>
2016-06-02 09:36:07 +0000

    winner algorithm

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -1,3 +1,40 @@
 
 
-const blocks = []
+const blocks = document.querySelectorAll('#board section');
+
+for (let i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('click', () => {
+        
+        console.log(blocks[i].getAttribute('id'))
+        
+    });
+}
+
+
+// [] -> [] -> integer
+const winner = function(winningCombo, board) {
+    return function(board) {
+        winningCombo.forEach((combo) => {
+          var won = combo.reduce((a,b) => {
+              return board[a] + board[b]
+          })    
+          console.log(won)
+        })
+        
+    }
+}
+
+const winningCombo = [
+    
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+const checkWinner = winner(winningCombo);
+
+const testBoard1 = []
+const testBoard2 = [1,2,3,4,5,6,7,8]
+
