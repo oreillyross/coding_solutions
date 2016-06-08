81cad5752349b79c6f7ac60605d217af5cf70fa0
Ross O'Reilly <oreillyross@gmail.com>
2016-06-08 08:48:56 +0000

    kth term

---
+++ codefights/kthTerm.js
@@ -0,0 +1,40 @@
+/*
+
+For the given integer n, consider an increasing sequence consisting of all positive integers that are either powers of n, or sums of distinct powers of n.
+
+Your task is to find the kth (1-based) number in this sequence.
+
+Example
+
+For n = 3 and k = 4, the output should be
+kthTerm(n, k) = 9.
+
+For n = 3, the sequence described above begins as follows: 1, 3, 4, 9, 10, 12, 13....
+The 4th number in this sequence is 9, which is the answer.Array
+
+*/
+
+
+// terse 
+
+kthTerm = _ = (n, k) => k && k%2 + n*_(n, k >> 1)
+
+// succint
+
+kthTerm = (n, k) => parseInt(k.toString(2), n)
+
+
+// More verbose
+kthTerm = (n, k, p) => {
+    s=[]
+    while(!s[k]) {
+        p=p*n||1
+        x=[p]
+        i=1
+        for(e of s) {
+            x[i++]= p+e
+        }
+        s.push(...x)
+    }
+    return s[k-1]
+}
--- codefights/taskMaker.js
+++ codefights/taskMaker.js
@@ -1,23 +1,22 @@
 
 function taskMaker(source, challengeId) {
 
-
+  source.forEach((line) => {
+    var rx = new RegExp('/\/\/DB ' + challengeId + '\/\/(.*)/g');
+    var correct = rx.exec(line)
+    console.log(correct)
+  })
+  
 
 }
-/*
-source = ["ans = 0",
+
+var source = ["ans = 0",
           "for i in range(n):",
           "    for j in range(n):",
           "    //DB 3//for j in range(1, n):",
           "    //DB 2//for j in range(n + 1):",
           "        ans += 1",
-          "return ans"]
-and challengeId = 3, the output should be
+          "return ans"];
+var challengeId = 3;
 
-taskMaker(source, challengeId) = ["ans = 0",
-                                  "for i in range(n):",
-                                  "    for j in range(1, n):",
-                                  "        ans += 1",
-                                  "return ans"]
-
-*/
+taskMaker(source,challengeId);
