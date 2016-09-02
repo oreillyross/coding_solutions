d87471ace581d3bedfca663cf416c7c8e47e05b8
Ross O'Reilly <oreillyross@gmail.com>
2016-09-02 11:22:44 +0000

    on superagent section

--- hwr/src/pages/Detail.js
+++ hwr/src/pages/Detail.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import Chance from 'chance';
+import ajax from 'superagent';
 
 class Detail extends React.Component {
     
     constructor(props) {
       super(props);
-      
-      const people = [];
-      for (let i = 0; i < 10; i++) {
-          people.push({
-            name: chance.first(),
-            country: chance.country({full: true})
-         })
-      }
-      this.state = { people }
+      this.state = { commits: [] };
+  
+    }
+    
+    componentWillMount() {
+        ajax.get('https://api.github.com/repos/facebook/react/commits')
+            .end((error, response) => {
+              if (!error && response) {
+                console.dir(response.body);
+                this.setState( { commits: response.body} );
+              } else {
+                console.log('There was an error getting data', error);
+              }
+            }
+        )
     }
     
    
@@ -22,9 +28,11 @@ class Detail extends React.Component {
       
         return (
             <div>
-               {this.state.people.map((person, index) => (
-                 <p>Hello, {person.name} from {person.country}</p>    
-               ))}
+               {this.state.commits.map((commit, index) => 
+               (<p key={index}>
+                 <strong>{commit.author ? commit.author.login : 'Anonymous'}</strong>: 
+                 <a href={commit.html_url}>{commit.commit.message}</a>.
+            </p>))}
             </div>
                
         );
