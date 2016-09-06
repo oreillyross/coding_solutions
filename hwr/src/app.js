e1736b15fd059eff716a77801b926dbe9491c9e1
Ross O'Reilly <oreillyross@gmail.com>
2016-09-06 11:03:14 +0000

    add router step

--- hwr/src/app.js
+++ hwr/src/app.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Detail from './pages/Detail';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
-ReactDOM.render(<Detail />, document.getElementById('app'));
+
+
+
+ReactDOM.render(
+    <Router>
+      <Route path='/' component={ Detail }/>
+    </Router>
+    , document.getElementById('app'));
--- hwr/src/pages/Detail.js
+++ hwr/src/pages/Detail.js
@@ -73,25 +73,10 @@ class Detail extends React.Component {
     }
 
 
-    showCommits() {
-        this.setState({
-            mode: 'commits'
-        });
+   selectMode(mode) {
+        this.setState({ mode })
     }
-
-    showForks() {
-        this.setState({
-            mode: 'forks'
-        });
-    }
-
-    showPulls() {
-        this.setState({
-            mode: 'pulls'
-        });
-    }
-
-
+    
     render() {
 
         let content;
@@ -107,9 +92,9 @@ class Detail extends React.Component {
         }
 
         return (<div>
-        <button onClick={this.showCommits.bind(this)}>Show Commits</button>
-        <button onClick={this.showForks.bind(this)}>Show Forks</button>
-        <button onClick={this.showPulls.bind(this)}>Show Pulls</button>
+        <button onClick={this.selectMode.bind(this, 'commits')}>Show Commits</button>
+        <button onClick={this.selectMode.bind(this, 'forks')}>Show Forks</button>
+        <button onClick={this.selectMode.bind(this, 'pulls')}>Show Pulls</button>
         {content}
     </div>);
     }
