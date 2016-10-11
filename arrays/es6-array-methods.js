f511cc0888b3e87f215c2d763f2fcd153a88a672
Ross O'Reilly <oreillyross@gmail.com>
2016-10-11 14:28:50 +0000

    es6 array methods

---
+++ arrays/es6-array-methods.js
@@ -0,0 +1,15 @@
+//TODO add examples for each new function added in es6
+
+Array.prototype.findIndex 
+//will give you the index of the first item from an array which matches 
+// the given predicate function.
+
+// But why return the index when you can just return the object you’re looking 
+// for straight up with 
+
+Array.prototype.find
+
+// Other new array methods include:
+
+Array.prototype.fill
+Array.prototype.copyWithin
