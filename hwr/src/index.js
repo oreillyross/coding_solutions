54b353f6343b512f38803a89f6404d14658d4424
Ross O'Reilly <oreillyross@gmail.com>
2016-10-13 14:37:02 +0000

    learning plumbing

---
+++ hwr/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+import React from 'react'
+
+
+
+class Detail extends React.Component {
+    render() {
+        return <p>This is React rendering HTML!</p>;
+    }
+}
+
+ReactDOM.render(<Detail/>, document.querySelector('#app'))
+
+
