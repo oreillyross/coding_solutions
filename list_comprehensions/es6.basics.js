ec2a1c71af1ba4e0bc47a6530bbb98e02397232a
Ross O'Reilly <oreillyross@gmail.com>
2017-01-04 11:33:38 +0000

    add functional sumAll

--- list_comprehensions/es6.basics.js
+++ list_comprehensions/es6.basics.js
@@ -1,7 +1,11 @@
 // recursive length function constructed from first principles using the 
 // new ES6 spread operator, destructuring of args.
-
+/*
 const length = ([first, ...rest]) =>
   first === undefined
     ? 0
     : 1 + length(rest);
+  */  
+// Sum all function written using rest operator and arrow functions, nice and functional
+
+const sumAll = (...numbers) => numbers.reduce((total,next) => total + next)
